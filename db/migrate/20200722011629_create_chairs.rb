adc3ca3ce4d7cc4c43dd8af3d1c75ae746f5c037
Alan <alanmannon@gmail.com>
2020-07-21 20:20:38 -0500

    initial commit, created chair model

---
+++ db/migrate/20200722011629_create_chairs.rb
@@ -0,0 +1,11 @@
+class CreateChairs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :chairs do |t|
+      t.string :color
+      t.integer :weight
+      t.string :style
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200722011915_addimagetochair.rb
@@ -0,0 +1,5 @@
+class Addimagetochair < ActiveRecord::Migration[6.0]
+  def change
+    add_column :chairs, :image_url, :string
+  end
+end
